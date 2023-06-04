549e2a33afd1b646b187e5629962ef6d6984f2a1
AlejandroCristM <alejandro.cristanchom@gmail.com>
2023-06-03 23:45:07 -0500

    fix: language issue solved

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,32 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from 'containers/Layout';
 import Home from 'pages/Home';
 
-import i18next from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import esTranslations from 'locales/es.json';
-import enTranslations from 'locales/en.json';
-
 function App() {
-  useEffect(() => {
-    i18next.use(initReactI18next).init({
-      resources: {
-        es: {
-          translation: esTranslations,
-        },
-        en: {
-          translation: enTranslations,
-        },
-      },
-      lng: 'es', // Default language (Spanish)
-      fallbackLng: 'en', // Fallback language (English)
-      interpolation: {
-        escapeValue: false,
-      },
-    });
-  }, []);
-
   return (
     <BrowserRouter>
       <Routes>
