8f2f32583c3745bb2760b1371dfe903f59f71468
AlejandroCristM <alejandro.cristanchom@gmail.com>
2023-06-29 22:43:25 -0500

    feat: navbar added - favicon ico

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Footer from 'components/Footer';
+import Footer from 'components/molecules/Footer';
 
 const Layout = () => (
   <div className='flex flex-col'>
